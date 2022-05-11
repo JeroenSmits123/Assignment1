5ab08361b5b227cdd48f37a29592fb1a2f550e24
JeroenSmits123 <105225141+JeroenSmits123@users.noreply.github.com>
2022-05-11 11:04:41 +0200

    Add files via upload

--- strings/main.py
+++ strings/main.py
@@ -14,4 +14,11 @@ goal_0 = 32
 goal_1 = 54
 scorers = goal_player1 +" "+ str(goal_0)+", " + goal_player2 + " "+str(goal_1)
 report = f'{goal_player1} scored in the {goal_0}nd minute \n{goal_player2} scored in the {goal_1}th minute'
-print(report)
+#print(report)
+
+player = "Frank Rijkaard"
+first_name  = player[:player.find(" ")]
+last_name_len = len(player[player.find(" ")+1:])
+name_short = player[:1] + ". " + player[-last_name_len:]
+chant = (f"{first_name}! "*len(first_name))[:-1]
+good_chant = chant[-1] != " "
